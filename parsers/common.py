e688d34ae6d1a0c953a26e309ee30116fa4a438a
Robbe Derks <robbe.derks@gmail.com>
2022-02-05 03:05:25 +0100

    wip: nxp parser

---
+++ parsers/common.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import os
+
+OHD_ROOT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+DATASHEET_DIR = os.path.join(OHD_ROOT_PATH, "datasheets")
