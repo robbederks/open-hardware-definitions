e39cfb362cb211da697ffdabcb66ea7c1d6183ab
Robbe Derks <robbe.derks@gmail.com>
2022-02-10 23:12:57 +0100

    add fields / enums to converts

--- parsers/svd/convert.py
+++ parsers/svd/convert.py
@@ -6,9 +6,11 @@ import sys
 sys.path.append("/home/robbe/open-hardware-definitions/parsers/svd/cmsis-svd/python")
 from cmsis_svd.parser import SVDParser
 
-from open_hardware_definitions import Device, Module, Register
+from open_hardware_definitions import Device, Module, Register, Field
 
-def convert(svd_dev):
+def convert(svd_dev_path):
+  svd_dev = SVDParser.for_xml_file(svd_dev_path).get_device()
+
   dev = Device(
     manufacturer=svd_dev.vendor,
     part_number=svd_dev.name,
@@ -25,6 +27,24 @@ def convert(svd_dev):
     )
 
     for reg in per.registers:
+      fields = []
+      for f in reg.fields:
+        # TODO: do we want descriptions here too?
+        evs = {}
+        if f.enumerated_values:
+          for ev in f.enumerated_values:
+            evs[ev.value] = ev.name
+
+        fields.append(Field(
+          name=f.name,
+          bit_offset=f.bit_offset,
+          bit_width=f.bit_width,
+          description=f.description,
+          read_allowed=('read' in f.access if f.access else None),
+          write_allowed=('write' in f.access if f.access else None),
+          enum_values=evs if len(evs.keys()) > 0 else None
+        ))
+
       mod.registers.append(Register(
         name=reg.name,
         addr=reg.address_offset,
@@ -32,18 +52,17 @@ def convert(svd_dev):
         description=reg.description,
         read_allowed=('read' in reg.access if reg.access else None),
         write_allowed=('write' in reg.access if reg.access else None),
-        reset_value=reg.reset_value
+        reset_value=reg.reset_value,
+        fields=fields
       ))
-
-      # TODO: register fields!
 
     dev.modules.append(mod)
 
   return dev
 
 if __name__ == "__main__":
-  # Parse XML
-  svd_dev = SVDParser.for_xml_file(sys.argv[1]).get_device()
-  dev = convert(svd_dev)
+  # Convert!
+  dev = convert(sys.argv[1])
 
+  # Dump out!
   print(dev.dump())
