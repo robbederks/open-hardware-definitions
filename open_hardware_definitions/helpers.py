4af708756acc1de51645b1cca9f18ce56d8079f8
Robbe Derks <robbe.derks@gmail.com>
2022-02-13 19:59:21 +0100

    add regions to NCP MPC5668x

--- open_hardware_definitions/helpers.py
+++ open_hardware_definitions/helpers.py
@@ -7,6 +7,9 @@ class CleanYAMLObject(yaml.YAMLObject):
   def to_yaml(cls, dumper, data):
     # Remove None fields from the dump
     data.__dict__ = dict((k, v) for k, v in data.__dict__.items() if v is not None)
+
+    # Remove empty list fields from the dump
+    data.__dict__ = dict((k, v) for k, v in data.__dict__.items() if not isinstance(v, list) or len(v) > 0)
     return super().to_yaml(dumper, data)
 
 class HexInt(int):
